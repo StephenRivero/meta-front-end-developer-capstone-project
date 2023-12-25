83908a10599fa01567fcc2eddaf941c02fcece33
Stephen Rivero <stephen.m.rivero@gmail.com>
2023-12-25 09:57:57 +0800

    Update responsive design of Banner Section

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -9,17 +9,14 @@ const Banner = () => {
       <div>
         <h2>Little Lemon</h2>
         <h3>Chicago</h3>
+        <img src={bannerImg} alt="Banner Img" className='banner-img-mobile' />
         <p>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</p>
         <Link to="/booking">
           <button aria-label='On Click'>Reserve a table</button>
         </Link>
+        {/* Banner Image */}
         <img src={bannerImg} alt="Banner Img" className='banner-img' />
       </div>
-
-      {/* Banner Image */}
-      {/* <div className='banner-img'>
-        <img src={bannerImg} alt="Banner Img" />
-      </div> */}
     </section>
   )
 }
