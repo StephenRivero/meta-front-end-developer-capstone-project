7881e398a27dfed2454e2e16d0cd368f27e110bc
Stephen Rivero <stephen.m.rivero@gmail.com>
2023-12-26 08:41:08 +0800

    Update responsive design of Banner section

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -10,7 +10,7 @@ const Banner = () => {
         <h2>Little Lemon</h2>
         <h3>Chicago</h3>
         <img src={bannerImg} alt="Banner Img" className='banner-img-mobile' />
-        <p>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</p>
+        <p>We are a family owned Mediterranean restaurant, <br className='mobile-hidden'/>focused on traditional recipes served with a modern twist.</p>
         <Link to="/booking">
           <button aria-label='On Click'>Reserve a table</button>
         </Link>
