c68cf04f23af70c8b81cfc12177a29dc33b23ba6
Stephen Rivero <stephen.m.rivero@gmail.com>
2023-12-30 11:28:23 +0800

    Update BookingForm component

--- src/components/BookingForm.js
+++ src/components/BookingForm.js
@@ -3,46 +3,49 @@ import React, { useState } from 'react'
 const BookingForm = (props) => {
 
   const [date, setDate] = useState("");
+  const [times, setTimes] = useState("");
+  const [guests, setGuests] = useState("");
+  const [occasion, setOccasion] = useState("");
 
-  // const handleSubmit = (e) => {
-  //   e.preventDefault();
-  // }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    props.SubmitForm(e);
+  }
 
   const handleDateChange = (e) => {
     setDate(e);
   }
 
+  const availableTimes = ["17:00", "18:00", "19:00", "20:00", "21:00", "22:00"]
+
   return (
     <section className='booking-form'>
-      <form action="">
+      <form onSubmit={handleSubmit}>
         <fieldset>
           {/* Book Date */}
           <div>
             <label htmlFor="book-date">Choose Date:</label>
-            <input id='book-date' type="date" value={date} onChange={(e) => handleDateChange(e.target.value)} required/>
+            <input id='book-date' type="date" value={date} onChange={e => handleDateChange(e.target.value)} required/>
           </div>
           {/* Book Time */}
           <div>
             <label htmlFor="book-time">Choose Time:</label>
-            <select name="" id="book-time">
+            <select name="" id="book-time" value={times} onChange={e => setTimes(e.target.value)} required>
               <option>Select a Time</option>
-              <option>17:00</option>
-              <option>18:00</option>
-              <option>19:00</option>
-              <option>20:00</option>
-              <option>21:00</option>
-              <option>22:00</option>
+              {
+                availableTimes.map(availableTimes => {return <option key={availableTimes}>{availableTimes}</option>})
+              }
             </select>
           </div>
           {/* Number of Guests */}
           <div>
             <label htmlFor="book-guests">Number of Guests:</label>
-            <input type="number" id='book-guests' min={1} placeholder="1" required/>
+            <input type="number" id='book-guests' min={1} placeholder="1" onChange={e => setGuests(e.target.value)} required/>
           </div>
           {/* Occasion */}
           <div>
             <label htmlFor="book-occasion">Occasion:</label>
-            <select name="" id="book-occasion">
+            <select name="" id="book-occasion" onChange={e => setOccasion(e.target.value)} required>
               <option value="">Birthday</option>
               <option value="">Anniversary</option>
             </select>
